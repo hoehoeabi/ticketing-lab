f934f5888baf89daf2567b7e136245f07b92c4e8
JungYongHyun <0203jyh00@naver.com>
2026-08-11 22:26:41 +0900

    feat: 프로젝트 초기 세팅(MySQL, Redis, gitignore)

---
+++ src/test/java/com/ticketing/ticketing_lab/TicketingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketing.ticketing_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
